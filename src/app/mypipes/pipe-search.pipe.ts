ca47f574533f1864b13c850e557e819f4418fd29
nisimben <59839685+nisimben@users.noreply.github.com>
2020-07-06 22:05:43 +0300

    Update 0.0.1

--- src/app/mypipes/pipe-search.pipe.ts
+++ src/app/mypipes/pipe-search.pipe.ts
@@ -16,6 +16,7 @@ export class PipeSearchPipe implements PipeTransform {
 
   transform(items: any[], searchText: string): any[] {
     if (!items || !searchText) return [];
+    searchText = searchText.toLowerCase;
     return items.filter(it => {
       return it.toLowerCase().startsWith(searchText);
     });
